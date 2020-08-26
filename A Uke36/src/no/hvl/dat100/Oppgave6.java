f356e224d99fe215f25fcb1f456734a9af2cca57
Aleksandar <Aleksandar@DESKTOP-FSEA27V.home>
2020-08-27 00:23:42 +0200

    Oppgave 6

--- A Uke36/src/no/hvl/dat100/Oppgave6.java
+++ A Uke36/src/no/hvl/dat100/Oppgave6.java
@@ -1,8 +1,26 @@
 package no.hvl.dat100;
+import java.util.Scanner;
 
 public class Oppgave6 {
 
 	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Skriv inn et tall:");
+		int tall = Integer.valueOf(scanner.nextLine());
+		int sum = 1;
+		
+		if (tall >= 0) {
+			
+			for (int i = 1; i <= tall; i++) {
+				
+				sum *= i;
+				//System.out.println(sum);
+			}
+			
+			System.out.println(tall + "! = " + sum);
+		}
 		
 		
 
